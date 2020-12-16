07c0447832d70d9bb33422830761af891708c012
SigExe <SIG96@protonmail.com>
2020-12-15 18:27:42 -0600

    changed my ip to my own ip, created class for api java object and created first pojo class

---
+++ src/test/java/pojo/Spartan.java
@@ -0,0 +1,46 @@
+package pojo;
+
+public class Spartan {
+
+    private String name;
+    private String gender;
+    private long phone;
+
+    public Spartan(String name, String gender, long phone) {
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public Spartan(){
+
+
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+
+}
